008e59f6f985ae469e490047a25dc0a2367009ed
Nix13 <nix1304@gmail.com>
2021-01-11 19:53:43 +0300

    done #3; needs tests

--- utils/vk/vk.py
+++ utils/vk/vk.py
@@ -1,11 +1,59 @@
 import asyncio
+from time import time
+from typing import Union
 
 from aiohttp import ClientSession, FormData
 
 
+class VkToken:
+    __slots__ = (
+        'cur_requests', 'max_rps',
+        '__token', '__last_req'
+    )
+
+    def __init__(self, token):
+        self.cur_requests = 0
+        self.max_rps = 20
+        self.__token = token
+        self.__last_req = 0
+
+    def __call__(self):
+        if self.cur_requests >= self.max_rps:
+            raise TypeError('too many requests')
+        self.cur_requests += 1
+        self.__last_req = int(time())
+        if self.__last_req < int(time()):
+            self.cur_requests = 0
+        return self.__token
+
+
+class VkTokenProvider:
+    __slots__ = (
+        'tokens',
+    )
+
+    def __init__(self, tokens: (Union[VkToken], VkToken)):
+        if type(tokens) == str:
+            self.tokens = [VkToken(tokens)]
+        else:
+            self.tokens = [VkToken(t) for t in tokens]
+
+    def obtain_token(self):
+        for t in self.tokens:
+            if t.cur_requests < t.max_rps:
+                return t()
+        else:
+            raise ValueError('no free tokens!')
+
+
 class VK:
+    __slots__ = (
+        'token_provider',
+        'v', 'session'
+    )
+
     def __init__(self, tokens: (str, list, tuple, set, frozenset), v='5.126'):
-        self.tokens = tokens
+        self.token_provider = VkTokenProvider(tokens)
         self.v = v
         self.session = ClientSession()
 
@@ -15,8 +63,7 @@ class VK:
     async def call_method(self, method, **params):
         params.update({'v': self.v})
 
-        if type(self.tokens) is str:
-            params.update({'access_token': self.tokens})
+        params.update({'access_token': self.token_provider.obtain_token()})
 
         async with self.session.post(
                 f'https://api.vk.com/method/{method}',
